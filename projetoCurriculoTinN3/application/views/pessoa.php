4104f60f9fe48817d267fef8cedd9c1e5086cf88
PedrohenriquebSantos <>
2017-11-08 21:12:32 -0200

    Front pessoas pronto, como o jair pediu!!!  pedro quem fez.

--- projetoCurriculoTinN3/application/views/pessoa.php
+++ projetoCurriculoTinN3/application/views/pessoa.php
@@ -10,6 +10,35 @@
                 <input class="form-control" type="text" name="nome" required="" placeholder="nome" maxlength="20">
                 <label style="margin-top:6px;">Telefone </label>
                 <input class="form-control" type="number" name="telefone" required="" placeholder="telefone" maxlength="20">
+                <label>Data De Nascimento</label>
+                <input class="form-control" type="date" name="dtnascimento" required="" placeholder="dtNascimento" maxlength="20">
+                <label>Sexo</label>
+                <input class="form-control" type="text" name="sexo" required="" placeholder="sexo" maxlength="20">
+                <label>E-mail</label>
+                <input class="form-control" type="email" name="email" required="" placeholder="email" maxlength="20">
+                <label>Descrição</label>
+                <input class="form-control" type="text" name="descricao" required="" placeholder="descriçao" maxlength="20">
+                <label>Estado Civil</label>
+                <input class="form-control" type="text" name="estadocivil" required="" placeholder="estadocivil" maxlength="20">
+                <label>CPF</label>
+                <input class="form-control" type="text" name="cpf" required="" placeholder="cpf" maxlength="20">
+                <label>Cidades</label>
+
+                <select class="form-control" name="cidade" required="">
+                    <?php foreach ($cidades as $u): ?>
+                        <option value="<?php echo $u->Cidades_id; ?>"><?php echo $u->cidades; ?></option>
+                    <?php endforeach; ?>
+                </select>
+
+                <label>Cargo</label>
+                
+                <select class="form-control" name="cargo" required="">
+                    <?php foreach ($cargos as $u): ?>
+                        <option value="<?php echo $u->Cargos_id; ?>"><?php echo $u->cargos; ?></option>
+                    <?php endforeach; ?>
+                </select>
+
+
                 <button class="btn btn-success btn-block" type="submit" style="margin-top:10px;">Salvar </button>
                 <button class="btn btn-danger btn-block" type="reset">Cancelar </button>
                 <?php echo form_close(); ?>
