527761916e38ba20a6b266a82e8b5b387f910b21
LeonardoIA1 <26637697+LeonardoIA1@users.noreply.github.com>
2017-11-24 19:59:51 -0300

    adicionado jquery no footer

--- projetoCurriculoTinN3/application/views/template/footer.php
+++ projetoCurriculoTinN3/application/views/template/footer.php
@@ -34,6 +34,7 @@
     </footer>
 </div>
 <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/jquery/jquery.min.js') ?>"></script>
 
 
 </body>
