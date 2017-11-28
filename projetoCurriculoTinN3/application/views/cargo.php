56616825debf5296381cf72235b7c73ac1e7f4d3
Valdimirr <>
2017-11-28 21:37:57 -0200

    Listar cargos, e exclui-los

--- projetoCurriculoTinN3/application/views/cargo.php
+++ projetoCurriculoTinN3/application/views/cargo.php
@@ -12,9 +12,35 @@
                 <button class="btn btn-success btn-block" type="submit" style="margin-top:10px;">Salvar </button>
                 <button class="btn btn-danger btn-block" type="reset">Limpar </button>
                 <a href="<?php base_url(); ?>home" class="btn btn-warning btn-block" type="reset">Voltar </a>
-                
+                 <a id="btn-lista" class="btn btn-warning btn-block"><i class="glyphicon glyphicon-th-list"></i>&nbsp;Listar</a>
                 <?php echo form_close(); ?>
             </div>
         </div>
     </div>
 </div>
+<div class="col-lg-offset-4 col-lg-push-0 col-md-4 col-md-offset-2 col-md-pull-0"></div>
+<div id="div-lista" class="hide col-lg-offset-4 col-lg-push-0 col-md-4 col-md-offset-2 col-md-pull-0"
+    style="margin-right: 100%">
+    <div class="">
+        <h2 class="">Lista de Cargos</h2>
+    </div>
+    <div class="">
+        <table class="col-lg-offset-4 col-lg-push-0 col-md-4 col-md-offset-2 col-md-pull-0">
+           
+            <tbody>
+                <?php foreach ($cargos as $u):  ?>
+                    <tr>
+                        <td><?php echo $u->cargos; ?></td>
+                        
+                        <td><a  href="<?php echo base_url('cargos/delete/'.$u->idcargos) ?>">Excluir </a> 
+                            
+                        </td>
+                        
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<br>
+<br
