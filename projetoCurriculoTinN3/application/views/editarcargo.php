62b95689d744a3430b692efedac10d33bf43ffc9
Valdimirr <>
2017-11-29 21:56:40 -0200

    Cidade e cargos edit tudo funcionando e atualizando, pessoas tbm.. Puxando variaveis etc.

--- projetoCurriculoTinN3/application/views/editarcargo.php
+++ projetoCurriculoTinN3/application/views/editarcargo.php
@@ -19,12 +19,13 @@
     ?>
         <div class="row" style="padding-top:18px;">
             <div class="col-lg-offset-4 col-lg-push-0 col-md-4 col-md-offset-2 col-md-pull-0">
-                <?php echo form_open('cargos/submit'); ?>
+                <?php echo form_open('cargos/update'); ?>
+                 <input type="hidden" name="hidden_id" value="<?php echo $cargos->idcargos ?>">
                 <fieldset>
                     <legend><strong>Editar Cargo</strong></legend>
                 </fieldset>
                 <label>Cargo </label>
-                <input class="form-control" type="text" name="cargos" required="required" placeholder="Cargo" maxlength="20">
+                <input class="form-control" type="text" name="cargos" required="required" value="<?php echo $cargos->cargos ?>" placeholder="Cargo" maxlength="20">
 
                 <button class="btn btn-success btn-block" type="submit" style="margin-top:10px;">Atualizar </button>
                 <button class="btn btn-danger btn-block" type="reset">Limpar </button>
--- projetoCurriculoTinN3/application/views/editarcidade.php
+++ projetoCurriculoTinN3/application/views/editarcidade.php
@@ -19,12 +19,13 @@
     ?>
         <div class="row" style="padding-top:18px;">
             <div class="col-lg-offset-4 col-lg-push-0 col-md-4 col-md-offset-2 col-md-pull-0">
-                <?php echo form_open('cidades/submit'); ?>
+                <?php echo form_open('cidades/update'); ?>
+                <input type="hidden" name="hidden_id" value="<?php echo $cidades->idcidades ?>">
                 <fieldset>
                     <legend><strong>Editar Cidade</strong></legend>
                 </fieldset>
                 <label>Cidade </label>
-                <input class="form-control" type="text" name="cidades" required="required" placeholder="Cidade" maxlength="20">
+                <input class="form-control" type="text" name="cidades" required="required" value="<?php echo $cidades->cidades ?>" placeholder="Cidade" maxlength="20">
 
                 <button class="btn btn-success btn-block" type="submit" style="margin-top:10px;">Atualizar </button>
                 <button class="btn btn-danger btn-block" type="reset">Limpar </button>
